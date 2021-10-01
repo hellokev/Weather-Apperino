1f7500dbdb6592ef163bd16ee8d6c62e6cc7e761
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-10-01 16:13:47 -0700

    Add some colors and text

--- Weather App/ContentView.swift
+++ Weather App/ContentView.swift
@@ -7,10 +7,26 @@
 
 import SwiftUI
 
+extension Color {
+    static let bg = Color("background")
+    static let fgPrimary = Color(red: 245, green: 245, blue: 245)
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.bg.ignoresSafeArea()
+            VStack {
+                Text("Weather App")
+                    .font(.system(size: 36, weight: .bold, design: .default))
+                    .foregroundColor(Color.fgPrimary)
+                    .padding(.bottom, 4)
+                
+                Text("Local weather near you")
+                    .font(.system(size: 20, weight: .light, design: .default))
+                    .foregroundColor(Color.fgPrimary)
+            }
+        }
     }
 }
 
